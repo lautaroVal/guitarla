898217b163dd3116e1c872a4978ff1935f6bef48
lautaroVal <valenzuelalautaro@yahoo.com>
2024-10-07 12:41:39 -0300

    Carrito persistente con LocalStorage - Recuperar los productos y mostrarlos

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,14 @@ import Header from "./components/Header";
 import { db } from './data/db';
 
 function App() {
+
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
  
   const [data, setData] = useState(db)  
-  const [cart, setCart] = useState([]) 
+  const [cart, setCart] = useState(initialCart) 
 
   const MAX_ITEMS = 5
 
