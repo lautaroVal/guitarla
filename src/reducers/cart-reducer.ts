215e23d8e7757adcb82b700127ef707c367cc250
lautaroVal <valenzuelalautaro@yahoo.com>
2026-07-06 20:53:46 -0300

    Definiendo el State inicial

--- src/reducers/cart-reducer.ts
+++ src/reducers/cart-reducer.ts
@@ -1,4 +1,5 @@
-import { Guitar } from "../types";
+import { db } from "../data/db";
+import { Guitar,CartItem } from "../types";
 
 export type CartActions =  
     {type: 'add-to-cart', payload: {item: Guitar}} |
@@ -6,3 +7,14 @@ export type CartActions =
     {type: 'increase-quantity', payload: {id: Guitar['id']}} |
     {type: 'decrease-quantity', payload: {id: Guitar['id']}} |
     {type: 'clear-cart'};
+
+
+export type CartState = {
+    data: Guitar[],
+    cart: CartItem[];
+};
+
+export const initialState : CartState = {
+    data: db,
+    cart: []
+}
